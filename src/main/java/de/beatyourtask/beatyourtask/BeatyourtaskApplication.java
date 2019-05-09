fc0f3acf97f3759daa3e2cc75ba5f8b5c7dd04d6
Jan <jdamm@posteo.de>
2019-05-09 15:13:19 +0200

    initial project setup

---
+++ src/main/java/de/beatyourtask/beatyourtask/BeatyourtaskApplication.java
@@ -0,0 +1,13 @@
+package de.beatyourtask.beatyourtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatyourtaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeatyourtaskApplication.class, args);
+    }
+
+}
