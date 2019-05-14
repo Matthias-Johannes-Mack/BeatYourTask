c4c819e5851dd4697189c629689ba8b03546436f
Jan <jdamm@posteo.de>
2019-05-14 08:13:26 +0200

    Removed code that was commented out

--- src/main/java/de/beatyourtask/beatyourtask/Controller/HomeController.java
+++ src/main/java/de/beatyourtask/beatyourtask/Controller/HomeController.java
@@ -2,17 +2,9 @@ package de.beatyourtask.beatyourtask.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-/*
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String showHome() {
-        return "home";
-    }
- */
 
  @GetMapping("/")
  public String showHome() {
