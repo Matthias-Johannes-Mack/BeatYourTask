3f6c7129306915f6b267e50bf60abcd56587d8bd
Angelo Gennaro <st156890@stud.uni-stuttgart.de>
2019-05-16 13:38:39 +0200

    Added AngeloGen to README.txt

--- src/main/java/de/beatyourtask/beatyourtask/controller/HomeController.java
+++ src/main/java/de/beatyourtask/beatyourtask/controller/HomeController.java
@@ -1,4 +1,4 @@
-package de.beatyourtask.beatyourtask.Controller;
+package de.beatyourtask.beatyourtask.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
