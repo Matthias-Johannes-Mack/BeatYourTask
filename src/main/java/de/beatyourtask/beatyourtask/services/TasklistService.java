04785ae68bc670933d8c9dff802494184a26b5c9
Jan <jdamm@posteo.de>
2019-05-20 23:06:56 +0200

    commented TasklistService

--- src/main/java/de/beatyourtask/beatyourtask/services/TasklistService.java
+++ src/main/java/de/beatyourtask/beatyourtask/services/TasklistService.java
@@ -8,26 +8,39 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 
-
+/**
+ * Class for handeling Tasklist in the database
+ */
 @Service
 public class TasklistService {
 
     @Autowired
     private TasklistRepository tasklistRepository;
 
+    /**
+     * Saves a tasklist in the database
+     * @param tasklist tasklist to be saved
+     * @return
+     */
     public Tasklist saveList(Tasklist tasklist) {
         return tasklistRepository.save(tasklist);
     }
 
+    /**
+     * get all tasklist from the database
+     * @return all tasklists
+     */
     @Transactional
     public List < Tasklist > getAllTaskLists() {
         return tasklistRepository.findAll();
     }
 
+    /**
+     * delets a tasklist
+     * @param id of the tasklist to be deleted
+     */
     @Transactional
     public void  deleteTasklistById(int id) {
         tasklistRepository.deleteById(id);
     }
-
-
 }
