7709153c16951f36a96b0d32658821005ec1a359
Jan <jdamm@posteo.de>
2019-05-20 22:58:18 +0200

    Refactored and commented Projektübersicht

--- src/main/java/de/beatyourtask/beatyourtask/controller/ProjectviewController.java
+++ src/main/java/de/beatyourtask/beatyourtask/controller/ProjectviewController.java
@@ -6,13 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * Controller for the ProjectView
+ * contains methods for controlling the Projectview
+ */
 @Controller
-public class ProjektansichtController {
+public class ProjectviewController {
 
     @Autowired
     private TasklistService tasklistService;
 
-
+    /**
+     * Loads the Projectview with all tasklists in the databes
+     * @param model model containing all the tasklist in the database
+     * @return projectview with all tasklist in database
+     */
     @GetMapping("/Project/")
     public String loadLists(Model model) {
         model.addAttribute("tasklists", tasklistService.getAllTaskLists());
