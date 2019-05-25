d7b968534a735d1b29b360b87b05a802dd6e22a6
Jan <jdamm@posteo.de>
2019-05-26 00:29:22 +0200

    Modal Submission for new new list added, curently only saves titel in DB, color needs to be added changed projectview controller so link from projectoverview works

--- src/main/java/de/beatyourtask/beatyourtask/controller/ProjectviewController.java
+++ src/main/java/de/beatyourtask/beatyourtask/controller/ProjectviewController.java
@@ -1,29 +1,71 @@
 package de.beatyourtask.beatyourtask.controller;
 
+import de.beatyourtask.beatyourtask.model.User;
 import de.beatyourtask.beatyourtask.services.TasklistService;
+import de.beatyourtask.beatyourtask.model.Tasklist;
+import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.RedirectView;
 
 /**
  * Controller for the ProjectView
  * contains methods for controlling the Projectview
  */
 @Controller
+
 public class ProjectviewController {
 
     @Autowired
     private TasklistService tasklistService;
 
+    @ModelAttribute(value = "newTaskListAttribute")
+    public Tasklist newTasklist()
+    {
+
+        return new Tasklist();
+    }
+
+
     /**
-     * Loads the Projectview with all tasklists in the databes
+     * Loads the Projectview with all tasklists in the database
      * @param model model containing all the tasklist in the database
      * @return projectview with all tasklist in database
      */
-    @GetMapping("/Project/")
-    public String loadLists(Model model) {
+    @GetMapping("/Project")
+    public ModelAndView loadLists(Model model) {
         model.addAttribute("tasklists", tasklistService.getAllTaskLists());
-        return "Projectview";
+
+        System.out.println("in Get");
+
+
+        String view = "Projectview";
+        return new ModelAndView(view, "command", model);
     }
+
+
+    /**
+     * Saves a new list in the database
+     * @param newTaskListAttribute new Tasklist Object from View
+     * @return reirekt to Projekt
+     */
+    @RequestMapping(value = "/addList", method = RequestMethod.POST)
+    public View addList(@ModelAttribute("newTaskListAttribute") Tasklist newTaskListAttribute) {
+
+        System.out.println("in Post");
+
+        tasklistService.saveList(newTaskListAttribute);
+
+        return new RedirectView("/Project");
+    }
+
+
+
+
+
 }
