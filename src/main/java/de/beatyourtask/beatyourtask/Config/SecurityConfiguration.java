06e1bcb4ad90fb091ebbc0b3dc20b483bc0b529b
Jan <jdamm@posteo.de>
2019-06-15 16:36:31 +0200

    Revert "Added ajax request when moving lists"
    This reverts commit 9dadffdb505c190d768aaea1d7f3ecc7ecef11c4.

--- src/main/java/de/beatyourtask/beatyourtask/Config/SecurityConfiguration.java
+++ src/main/java/de/beatyourtask/beatyourtask/Config/SecurityConfiguration.java
@@ -62,9 +62,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         // Deaktiviert header security. Ermöglicht Nutzung der H2 Console.
         http.headers().frameOptions().sameOrigin().disable();
-
-        //Deaktivierung csrf damit ajax requests funktionieren
-        http.csrf().disable();
     }
 
     @Override
@@ -84,6 +81,5 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         return bCryptPasswordEncoder;
     }
-
 
 }
