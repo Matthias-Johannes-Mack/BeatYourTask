b32804ff413096c9fdd654ec36a98b985c9edd65
Jan <jdamm@posteo.de>
2019-06-19 23:50:28 +0200

    tasklists load in the rigth order

--- src/main/java/de/beatyourtask/beatyourtask/controller/ProjectviewController.java
+++ src/main/java/de/beatyourtask/beatyourtask/controller/ProjectviewController.java
@@ -17,6 +17,8 @@ import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Controller for the ProjectView
@@ -36,7 +38,6 @@ public class ProjectviewController {
     @ModelAttribute(value = "newTaskListAttribute")
     public Tasklist newTasklist()
     {
-
         return new Tasklist();
     }
 
@@ -49,11 +50,38 @@ public class ProjectviewController {
      * */
     @GetMapping("/Project{ProjectId}")
     public ModelAndView loadListsID(Model model, @RequestParam("ProjectId") Integer id) {
-        model.addAttribute("tasklists", projectService.findById(id).getLists());
-        System.out.println(id);
-        //Project?ProjectId=10
-        System.out.println("in /ProjectID");
+        //for loading the lists in the rigth order
+        Project currentProject = projectService.findById(id);
+        List<Tasklist> unSortedTasklists =currentProject.getLists();
+        List<Integer> order = currentProject.getOrders();
+        List<Tasklist> sortedTasklists = new ArrayList<Tasklist>();
+        List<Tasklist> listsToRemoveFromUnsorted = new ArrayList<Tasklist>();
 
+        for (Integer currentId : order) {
+            for (Tasklist currenTasklist: unSortedTasklists) {
+                if(currenTasklist.getListId() == currentId) {
+                    sortedTasklists.add(currenTasklist);
+                    listsToRemoveFromUnsorted.add(currenTasklist);
+                    break;
+                }
+            }
+        }
+        unSortedTasklists.removeAll(listsToRemoveFromUnsorted);
+
+        System.out.println("unsorted Tasklist"+unSortedTasklists);
+        System.out.println("Sorted Tsklist: "+sortedTasklists);
+
+        //if some of list ids were not allready saved in the order these tasklist will be loaded at the end
+        //e.g. when a list is added
+        for (Tasklist currenTasklist: unSortedTasklists) {
+                sortedTasklists.add(currenTasklist);
+        }
+        System.out.println("Sorted Tsklist: "+sortedTasklists);
+
+
+        model.addAttribute("tasklists", sortedTasklists);
+        System.out.println(id);
+        System.out.println("in /ProjectID");
 
         String view = "Projectview";
         return new ModelAndView(view, "command", model);
@@ -117,7 +145,6 @@ public class ProjectviewController {
         return "redirect:/Project?ProjectId="+projectId;
     }
 
-
     /**
      * Deletes a list in the database
      * @param newTaskListAttribute new Tasklist Object from View
@@ -133,8 +160,5 @@ public class ProjectviewController {
 
         return "redirect:/Project?ProjectId="+projectId;
     }
-
-
-
 
 }
