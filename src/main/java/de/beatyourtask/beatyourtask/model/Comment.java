1690dcd33f60c16d17cd623694cf10f158c0e0c8
Angelo Gennaro <st156890@stud.uni-stuttgart.de>
2019-07-02 23:47:26 +0200

    test commit for new branch

--- src/main/java/de/beatyourtask/beatyourtask/model/Comment.java
+++ src/main/java/de/beatyourtask/beatyourtask/model/Comment.java
@@ -2,13 +2,73 @@ package de.beatyourtask.beatyourtask.model;
 
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Comment {
 
+    @Id
+    @GeneratedValue
+    private Integer id;
 
-    public Comment(){
+    private String message;
+    private String createDate;
 
+    @OneToOne
+    private User author;
+
+    @OneToOne
+    private Task ownerTask;
+
+
+    // Constructor
+
+    public Comment() {
     }
 
+
+    // Getters and Setters
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public Task getOwnerTask() {
+        return ownerTask;
+    }
+
+    public void setOwnerTask(Task ownerTask) {
+        this.ownerTask = ownerTask;
+    }
 }
